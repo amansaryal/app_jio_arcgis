550eefaba5c1f20289108eb0d2e988771a3041e1
Aman Saryal <amansaryal@gmil.com>
2016-08-19 15:26:06 +0530

    Basic Login Page created

--- app/src/main/java/com/example/android/app_jio_arcgis/Login.java
+++ app/src/main/java/com/example/android/app_jio_arcgis/Login.java
@@ -2,6 +2,7 @@ package com.example.android.app_jio_arcgis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+    }
+    public void onClick(){
+        Toast.makeText(this,"logging in...",Toast.LENGTH_SHORT).show();
     }
 }
